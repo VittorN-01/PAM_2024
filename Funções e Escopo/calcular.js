75a0d73688618517465a33dafcc50bd265fd63b6
Vittor Nascimento Sirqueira <144181234+VittorN-01@users.noreply.github.com>
2024-10-02 20:39:22 -0300

    Add files via upload

--- Funções e Escopo/calcular.js
+++ Funções e Escopo/calcular.js
@@ -1,18 +1,3 @@
-document.getElementById('calcForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
-
-    var v1 = parseFloat(document.getElementById('v1').value);
-    var v2 = parseFloat(document.getElementById('v2').value);
-
-    var resultados = `
-        Soma: ${soma(v1, v2)}<br>
-        Subtração: ${subtrair(v1, v2)}<br>
-        Multiplicação: ${multiplicacao(v1, v2)}<br>
-        Divisão: ${divisao(v1, v2)}<br>
-    `;
-    document.getElementById('result').innerHTML = resultados;
-});
-
 function soma(a, b) {
     return a + b;
 }
@@ -26,5 +11,5 @@ function multiplicacao(a, b) {
 }
 
 function divisao(a, b) {
-    return b !== 0 ? a / b : 'Divisão por zero!';
+    return a / b;
 }
